2a0cf9d26e66113d0c557b6a985cc9744c4e2e25
Carlos Eduardo Olivieri <caduolivieri1984@gmail.com>
2026-06-29 23:22:36 -0300

    Ajustes.

---
+++ Indt.Teste.PropostaService/tests/Indt.Teste.PropostaService.IntegrationTests/Fixtures/DatabaseFixture.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace Indt.Teste.PropostaService.IntegrationTests.Fixtures;
+
+public class DatabaseFixture : IAsyncLifetime
+{
+    public const string ConnectionString =
+        "Server=localhost;" +
+        "User Id=sa;" +
+        "Password=B@nana123;" +
+        "Encrypt=False;" +
+        "TrustServerCertificate=True;";
+
+    private static async Task ExecuteScriptAsync()
+    {
+        await using var connection = new SqlConnection(ConnectionString);
+
+        await connection.OpenAsync();
+
+        await using var command = new SqlCommand(DatabaseScripts.CreateDB, connection);
+
+        await command.ExecuteNonQueryAsync();
+
+        await using var command2 = new SqlCommand(DatabaseScripts.Setup, connection);
+
+        await command2.ExecuteNonQueryAsync();
+    }
+
+    public async ValueTask InitializeAsync() =>
+        await ExecuteScriptAsync();
+
+    public ValueTask DisposeAsync()
+    {
+        GC.SuppressFinalize(this);
+        return ValueTask.CompletedTask;
+    }
+}
